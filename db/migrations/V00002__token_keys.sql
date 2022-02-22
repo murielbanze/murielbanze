bb2fa790565c54f59ffefe308185105ca5fd0e9a
wwootenfoundry <wwooten@foundrydigital.com>
2022-02-22 11:55:22 -0500

    start token

--- db/migrations/V00002__token_keys.sql
+++ db/migrations/V00002__token_keys.sql
@@ -5,12 +5,14 @@ CREATE TABLE token (token_id INT,
                     token_symbol VARCHAR(256) NOT NULL,
                     eth_contract_hash TIMESTAMP);
 
-
 INSERT INTO token
 VALUES 
-(0, "Bitcoin", "BTC", NULL),
-(1, "Ethereum", "ETH", NULL),
-(2, "Solana", "SOL", NULL),
-(3, "Near", "NEAR", NULL),
-(4, "Helium", "HNT", NULL), 
-(5, "Stacks", "STX", NULL);
+(0, "Bitcoin", "BTC"),
+(1, "Ethereum", "ETH"),
+(2, "Solana", "SOL"),
+(3, "Near", "NEAR"),
+(4, "Helium", "HNT"), 
+(5, "Stacks", "STX"), 
+(6, "Litecoin", "LTC"),
+(7, "Dogecoin", "DOGE"),
+(8, "Bitcoin Cash", "BCH";
